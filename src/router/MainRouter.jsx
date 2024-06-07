d1f2bbe620364804ec961eef8d4d80fe8692960b
Luifer <luifer01ortegaperez@gmail.com>
2024-06-07 18:38:37 -0500

    feat: creación del componente 'PageNotFound'
    Detalles:
    
    - Se añadió el componente 'PageNotFound' para manejar y mostrar de manera adecuada las rutas inexistentes.

--- src/router/MainRouter.jsx
+++ src/router/MainRouter.jsx
@@ -1,13 +1,18 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { Routes, Route } from "react-router-dom";
+import PageNotFounde from "../components/PageNotFounde";
 
 const MainRouter = () => {
     return (
         <>
             <Header />
+            <Routes>
+                <Route path="*" element={<PageNotFounde />} />
+            </Routes>
             <Footer />
         </>
-    )
-}
+    );
+};
 
-export default MainRouter
+export default MainRouter;
