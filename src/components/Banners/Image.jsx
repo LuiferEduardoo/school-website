437e766b5ac3aa135cebfac6ff4d76bc45d96f64
Luifer <luifer01ortegaperez@gmail.com>
2024-06-12 18:28:40 -0500

    fix: corrección de error en el componente Banner
    Detalles:
    
    - Se solucionó un error en el componente 'Banner' que impedía la visualización de las imágenes, asegurando que ahora se muestren correctamente.

--- src/components/Banners/Image.jsx
+++ src/components/Banners/Image.jsx
@@ -2,7 +2,7 @@ const Image = ({slides, currentIndex, styleImage}) => {
     return (
         <>
             <div 
-                style={{backgroundImage: `url(${slides[currentIndex].url})` }} 
+                style={{backgroundImage: `url(${slides[currentIndex]?.url})` }} 
                 className={`w-full h-full ${styleImage ? styleImage : ''} bg-center bg-cover duration-500`}
             />
         </>
--- src/router/MainRouter.jsx
+++ src/router/MainRouter.jsx
@@ -2,6 +2,7 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { Routes, Route } from "react-router-dom";
 import Admissions from "../pages/Admissions";
+import Contact from "../pages/Contact";
 import PageNotFounde from "../components/PageNotFounde";
 
 const MainRouter = () => {
@@ -10,6 +11,7 @@ const MainRouter = () => {
             <Header />
             <Routes>
                 <Route path="/admisiones/*" element={<Admissions />} />
+                <Route path="/contacto" element={<Contact />} />
                 <Route path="*" element={<PageNotFounde />} />
             </Routes>
             <Footer />
